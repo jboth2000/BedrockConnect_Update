b4bcdce16ae509a3f44c1fcb2a0444e472f58ce0
jboth2000 <108500655+jboth2000@users.noreply.github.com>
2022-07-16 19:28:31 -0600

    Update BedrockConnectUpdate.sh

--- BedrockConnectUpdate.sh
+++ BedrockConnectUpdate.sh
@@ -8,17 +8,18 @@ URL=$(curl -fs -o/dev/null -w %{redirect_url} https://github.com/Pugmatt/Bedrock
 #if missing Bedrockversion.txt, this will create the file
 if [[ ! -f Bedrockversion.txt ]]
 then
-    touch Bedrockversion.txt
+	touch Bedrockversion.txt
+else
+
+	#reads Bedrockversion.txt file into veriable #file
+	while read line; do file=$line; done < Bedrockversion.txt
+
+	#compares URLS for the curl and $file. If different, this will remove and download the latest version
+	if [ "$URL" != $file ]; then
+		systemctl stop BedrockConnect.service	#Comment out if you are not running the BedrockConnect as a service
+		rm BedrockConnect-1.0-SNAPSHOT.jar
+		wget $URL/BedrockConnect-1.0-SNAPSHOT.jar
+		systemctl start BedrockConnect.service   #Comment out if you are not running the BedrockConnect as a service
+		echo $URL > Bedrockversion.txt
+	fi
 fi 
-
-#reads Bedrockversion.txt file into veriable #file
-while read line; do file=$line; done < Bedrockversion.txt
-
-#compares URLS for the curl and $file. If different, this will remove and download the latest version
-if [ "$URL" != $file ]; then
-	systemctl stop BedrockConnect.service	#Comment out if you are not running the BedrockConnect as a service
-	rm BedrockConnect-1.0-SNAPSHOT.jar
-	wget $URL/BedrockConnect-1.0-SNAPSHOT.jar
-	systemctl start BedrockConnect.service   #Comment out if you are not running the BedrockConnect as a service
-	echo $URL > Bedrockversion.txt
-fi
